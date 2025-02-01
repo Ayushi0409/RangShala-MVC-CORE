a073916d15d4e036920941cffe88bd7305c3e637
Ayushi0409 <ayushibabariya4@gmail.com>
2025-02-01 16:56:41 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RangShala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
